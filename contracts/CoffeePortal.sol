95b9cf0cca926ba2490776f147d5fff604650a5c
KowalewskiPawel <53332984+KowalewskiPawel@users.noreply.github.com>
2022-01-25 22:34:35 +0100

    Update CoffeePortal.sol

--- contracts/CoffeePortal.sol
+++ contracts/CoffeePortal.sol
@@ -8,10 +8,7 @@ contract CoffeePortal {
     uint256 totalCoffee;
 
     address payable public owner; 
-
-    /*
-     * A little magic, Google what events are in Solidity!
-     */
+    
     event NewCoffee(
         address indexed from,
         uint256 timestamp,
@@ -20,50 +17,26 @@ contract CoffeePortal {
     );
 
     constructor() payable {
-        console.log("Yo! Smart Contract");
-
-        // user who is calling this function address
         owner = payable(msg.sender);
     }
 
-    /*
-     * I created a struct here named Coffee.
-     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
-     */
     struct Coffee {
-        address giver; // The address of the user who buys me a coffee.
-        string message; // The message the user sent.
-        string name; // The name of the user who buys me a coffee.
-        uint256 timestamp; // The timestamp when the user buys me a coffee.
+        address giver;
+        string message;
+        string name;
+        uint256 timestamp;
     }
 
-    /*
-     * I declare variable coffee that lets me store an array of structs.
-     * This is what lets me hold all the coffee anyone ever sends to me!
-     */
     Coffee[] coffee;
 
-    /*
-     * I added a function getAllCoffee which will return the struct array, coffee, to us.
-     * This will make it easy to retrieve the coffee from our website!
-     */
     function getAllCoffee() public view returns (Coffee[] memory) {
         return coffee;
     }
 
-    // Get All coffee bought
     function getTotalCoffee() public view returns (uint256) {
-        // Optional: Add this line if you want to see the contract print the value!
-        // We'll also print it over in run.js as well.
-        console.log("We have %d total coffee recieved ", totalCoffee);
         return totalCoffee;
     }
 
-    /*
-     * You'll notice I changed the buyCoffee function a little here as well and
-     * now it requires a string called _message. This is the message our user
-     * sends us from the front end!
-     */
     function buyCoffee(
         string memory _message,
         string memory _name,
@@ -75,9 +48,6 @@ contract CoffeePortal {
         totalCoffee += 1;
         console.log("%s has just sent a coffee!", msg.sender);
 
-        /*
-         * This is where I actually store the coffee data in the array.
-         */
         coffee.push(Coffee(msg.sender, _message, _name, block.timestamp));
 
         (bool success, ) = owner.call{value: _payAmount}("");
