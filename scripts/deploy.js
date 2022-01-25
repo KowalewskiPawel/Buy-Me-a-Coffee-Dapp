6aae73c5ae21b39d7c154cd3f30e958ff09681da
KowalewskiPawel <53332984+KowalewskiPawel@users.noreply.github.com>
2022-01-25 22:30:57 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,7 @@
 const main = async () => {
   const [deployer] = await hre.ethers.getSigners();
   const accountBalance = await deployer.getBalance();
-
-  console.log("Deploying contracts with account: ", deployer.address);
+  
   console.log("Account balance: ", accountBalance.toString());
 
   const Token = await hre.ethers.getContractFactory("CoffeePortal");
